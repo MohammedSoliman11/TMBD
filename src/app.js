4f8a503c44f98a8118fe18ec1cf4819b089c2f95
mohammedsoliman11 <mohammed.soliman@zimozi.co>
2025-03-30 05:36:19 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('TMDB Express API is running...');
+});
+
+module.exports = app;
